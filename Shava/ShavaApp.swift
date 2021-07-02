30a68051a35d4ab084f8ef0b756cabb37c8384f9
ZolotAlice <zolotalice@gmail.com>
2021-07-02 16:56:57 +0300

    Initial Commit

---
+++ Shava/ShavaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShavaApp.swift
+//  Shava
+//
+//  Created by Alice Zolotareva on 02.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct ShavaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
