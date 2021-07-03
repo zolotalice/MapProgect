5666610877fb6bcff48c062e14541163d6f60259
ZolotAlice <zolotalice@gmail.com>
2021-07-03 18:27:29 +0300

    first commit

--- Shava/ContentView.swift
+++ Shava/ContentView.swift
@@ -6,11 +6,11 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Home()
     }
 }
 
@@ -18,4 +18,66 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+struct Home: View{
+    //latitude широта/ longitude долгота
+    
+    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 13.086, longitude: 80.2769), latitudinalMeters: 10000, longitudinalMeters: 10000)
+    @State var tracking: MapUserTrackingMode = .follow
+    
+    @State var manager = CLLocationManager()
+    @StateObject var managerDelegate = locationDelegate()
+    var body: some View{
+        VStack{
+            
+            Map(coordinateRegion: $region, interactionModes: .all, showsUserLocation: true, userTrackingMode: $tracking, annotationItems: managerDelegate.pins){pin in
+                MapPin(coordinate: pin.location.coordinate, tint: .blue)
+            }
+        }
+        .onAppear{
+            
+            manager.delegate = managerDelegate
+        }
+    }
+}
+
+class locationDelegate: NSObject, ObservableObject, CLLocationManagerDelegate{
+    
+    @Published var pins : [Pin] = []
+    //checking autorization status
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if manager.authorizationStatus == .authorizedWhenInUse{
+            print("authorized")
+            
+            if manager.accuracyAuthorization != .fullAccuracy{
+                print("reduced Accuracy")
+                
+                manager.requestTemporaryFullAccuracyAuthorization(withPurposeKey: "Location"){ (err) in
+                    
+                    if err != nil {
+                        print(err!)
+                        return
+                    }
+                }
+            }
+            
+            manager.startUpdatingLocation()
+        }
+        else{
+            print("not autorized")
+            
+            manager.requestWhenInUseAuthorization()
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        pins.append(Pin(location: locations.last!))
+    }
+}
+
+struct Pin: Identifiable {
+    var id = UUID().uuidString
+    var location : CLLocation
 }
